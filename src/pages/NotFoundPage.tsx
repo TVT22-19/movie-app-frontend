b1f54a6a8e3022f3c76eff9bea44832bedae0386
syorito-hatsuki <enderdron3@gmail.com>
2023-11-10 08:29:29 +0200

    Added path info to 404, trying debug why GHP always throw NotFoundPage.tsx

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,19 @@
+import {useLocation} from "react-router-dom";
+import {useEffect, useState} from "react";
+
 export default function NotFoundPage() {
+
+    const location = useLocation()
+    const [currentPath, setCurrentPath] = useState("")
+
+    useEffect(() => {
+        setCurrentPath(location.pathname);
+    }, [location]);
+
     return (
         <>
             <h1>404 - Page not found :(</h1>
+            <h3>Path: {currentPath}</h3>
         </>
     )
 }
