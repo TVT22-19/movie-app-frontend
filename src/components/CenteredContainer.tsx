d9a50198da72546b9c77e3efdcb2bd5fa04ddb2e
syorito-hatsuki <enderdron3@gmail.com>
2023-11-10 08:03:34 +0200

    Added global component example

---
+++ src/components/CenteredContainer.tsx
@@ -0,0 +1,21 @@
+import {CentredContainerProps} from "./types";
+import {CSSProperties} from "react";
+
+export default function CenteredContainer(props: CentredContainerProps) {
+
+    const containerStyle: CSSProperties = {
+        height: "100svh",
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        alignContent: "center",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+
+    return (
+        <div style={containerStyle}>
+            {props.children}
+        </div>
+    )
+}
