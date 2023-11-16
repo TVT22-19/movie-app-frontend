1ccc4abf0cb0362f46f1c54f1e37fd19f18c5191
syorito-hatsuki <enderdron3@gmail.com>
2023-11-16 14:38:41 +0200

    Authorization hook

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,36 @@
+import {createContext, useContext, useState} from "react";
+import {AuthContent, AuthProviderProps} from "./types";
+
+const AuthContext = createContext<AuthContent>({
+    getToken(): string | undefined {
+        return undefined;
+    },
+    setToken(token: string): void {
+    },
+    isAuthorized: false
+});
+
+export const AuthProvider = (props: AuthProviderProps) => {
+
+    const [_token, _setToken] = useState(props.userToken);
+
+    const isAuthorized = !(!getToken() || getToken() === "");
+
+    function setToken(token: string) {
+        _setToken(token)
+        localStorage.setItem("token", token)
+    }
+
+    function getToken(): string | undefined {
+        return _token
+    }
+
+    return (
+        <AuthContext.Provider value={{getToken, setToken, isAuthorized}}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => useContext(AuthContext);
