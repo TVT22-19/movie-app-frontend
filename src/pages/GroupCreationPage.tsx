34318226ae1f8e55acdfdc41baa5560265cd9f24
asrii00 <t2rias00@students.oamk.fi>
2023-11-27 12:31:39 +0200

    added avatar url field to group creation

--- src/pages/GroupCreationPage.tsx
+++ src/pages/GroupCreationPage.tsx
@@ -5,40 +5,41 @@ import {useState} from "react";
 
 export default function LoginPage() {
    
-
     const [groupname, setGroupname] = useState<string>("")
-    const [groupdesc, setGroupDescription] = useState<string>("")
-   
+    const [groupDescription, setGroupDescription] = useState<string>("")
+    const [groupAvatar, setGroupAvatar] = useState<string>("")
 
-   
 
-    function createGroup(groupname: string, groupdesc: string) {
+    function createGroup(groupname: string, groupdesc: string, groupavatar: string) {
         
 
         if (groupname.trim() === "" || groupdesc.trim() === "") {
-            console.log("Some fields are empty")
-            return
+            console.log("Some required fields are empty")
+            return //
         }
         //limit description length?
 
     }
 
     return (
-        <Container maxWidth="xs">
+        <Container maxWidth="xs"> 
             <Card>
                 <Stack padding={2} spacing={2}>
                     <Typography variant="h4" align="center">Create New Group</Typography>
                     <TextField label="Group name" variant="outlined" type="text"
                                onChange={(event) => setGroupname(event.target.value)}/>
-
                     <TextField label="Group description" variant="outlined" type="text" multiline rows={4}
                                onChange={(event) => setGroupDescription(event.target.value)}/>
+                    <TextField label="Group avatar URL (optional)" variant="outlined" type="text"
+                               onChange={(event) => setGroupAvatar(event.target.value)}/>
 
                     <Button variant="contained" sx={{flexGrow: 9}}
-                                onClick={() => createGroup(groupname, groupdesc)}>Create</Button>
+                                onClick={() => createGroup(groupname, groupDescription, groupAvatar)}>Create</Button>
                         
                 </Stack>
             </Card>
         </Container>
     )
 }
+
+//maybe the card should be wider
