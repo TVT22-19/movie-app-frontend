68f6aedbaebef50831979eb44c9274b7cd135753
syorito-hatsuki <enderdron3@gmail.com>
2023-11-28 11:36:20 +0200

    Extracted Sidebar and added group invite list

---
+++ src/pages/root/components/Sidebar.tsx
@@ -0,0 +1,74 @@
+import {
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    Typography
+} from "@mui/material";
+import {Check, Close, Group} from "@mui/icons-material";
+import {useGroupInvites, useGroups} from "../../../services/groups.ts";
+import {useAuth} from "../../../hooks/useAuth.tsx";
+
+export default function Sidebar() {
+
+    const drawerWidth = 240;
+
+    const {isAuthorized} = useAuth()
+
+    const {data: groups} = useGroups(isAuthorized)
+    const {data: groupInvites} = useGroupInvites(isAuthorized)
+
+    return (
+        <Drawer variant="permanent" anchor="right" sx={{
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+                width: drawerWidth,
+                boxSizing: 'border-box'
+            },
+        }}>
+            <Toolbar/>
+            <Box sx={{overflow: 'auto'}}>
+                <List>
+                    {isAuthorized &&
+                        <>
+                            {groupInvites?.map(group => (
+                                <ListItem key={group.id}>
+                                    <ListItemIcon>
+                                        <Group/>
+                                    </ListItemIcon>
+                                    <ListItemText primary={
+                                        <Typography overflow="hidden"
+                                                    textOverflow="ellipsis">{group.name}</Typography>
+                                    }/>
+                                    <IconButton style={{padding: 4}}>
+                                        <Check/>
+                                    </IconButton>
+                                    <IconButton edge="end" style={{padding: 4}}>
+                                        <Close/>
+                                    </IconButton>
+                                </ListItem>
+                            ))}
+                            <Divider/>
+                            {groups?.map(group => (
+                                <ListItem key={group.id} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            <Group/>
+                                        </ListItemIcon>
+                                        <ListItemText primary={group.name}/>
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </>
+                    }
+                </List>
+            </Box>
+        </Drawer>
+    )
+}
