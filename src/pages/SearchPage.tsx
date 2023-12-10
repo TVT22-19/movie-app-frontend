7292eca7e1f36c8336c6f787f499c9f18a77732e
asrii00 <t2rias00@students.oamk.fi>
2023-12-10 23:54:09 +0200

    added filtering and fixes

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -12,7 +12,7 @@ import {
     useTheme,
     Rating
 } from '@mui/material';
-import { useQuery } from '@tanstack/react-query';
+
 
 //will be moved
 interface Movie {
@@ -40,43 +40,50 @@ const fetchMovies = async (query: string) => {
     if (!query) {
         return [];
     }
-    const response = await fetch(`${hostUrl}/moviedb/search/${query}`);
+    const response = await fetch(`${hostUrl}/moviedb/search/movie/${query}`);
     if (response.status !== 200) throw new Error((await response.json()).message);
     const data = await response.json();
     return data.results;
 }
 
 
-
 const SearchPage: React.FC = () => {
     const [searchQuery, setSearchQuery] = useState<string>('');
     const [year, setYear] = useState<number>(0);
     const [value, setValue] = React.useState<number | null>(2);
-    const [timeToSearch, setTimeToSearch] = useState<boolean>(false);
-
-    const useFetchMovies = (query: string) => useQuery<Movie[] | undefined, Error>({
-        queryKey: ["fetchmovies", query],
-        queryFn: () => fetchMovies(query),
-        enabled: timeToSearch,
-    });
-
-    const { data: movies, isLoading, isError } = useFetchMovies(searchQuery);
+    const [movies, setMovies] = useState<Movie[] | undefined>(undefined);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isError, setIsError] = useState<boolean>(false);
 
 
-    //add actual logic for when the search is performed
+    const handleSearch = async () => {
+        try {
+            setIsLoading(true);
+            const data = await fetchMovies(searchQuery);
+    
+            // filter movies by year
+            const filteredMoviesByYear = year !== 0
+                ? data.filter((movie: Movie) => new Date(movie.release_date).getFullYear() === year)
+                : data;
+    
+            // filter movies by minimum rating
+            const filteredMovies = value !== null
+                ? filteredMoviesByYear.filter((movie: Movie) => movie.vote_average >= value * 2) // scaling to convert 5 stars to 0-10 scale
+                : filteredMoviesByYear;
+    
+            setMovies(filteredMovies);
 
-    const handleSearch = () => {
-
-        setTimeToSearch(true);
-        
-
+        } catch (error) {
+            setIsError(true);
+        } finally {
+            setIsLoading(false);
+        }
     };
 
-    React.useEffect(() => {
-        if (movies) {
-            setTimeToSearch(false);
-        }
-    }, [timeToSearch]);
+    const handleClearYear = () => {
+        setYear(0);
+    };
+
 
     const theme = useTheme();
 
@@ -100,13 +107,27 @@ const SearchPage: React.FC = () => {
 
                     <Grid item xs={6} sm={3} md={2}>
                         <TextField
-                            type="number"
+                            type="text"
                             label="Year"
                             variant="outlined"
                             fullWidth
-                            value={year}
+                            value={year !== 0 ? year : ''}
                             onChange={(e) => setYear(Number(e.target.value))}
+                            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                         />
+
+
+                        {/*ugly, ignore that for now*/}
+                        {year !== 0 && (
+
+                            <Button
+                                variant="outlined"
+
+                                onClick={handleClearYear}
+                            >
+                                Clear Year
+                            </Button>
+                        )}
                     </Grid>
                     <Grid item xs={6} sm={3} md={2}>
                         <Typography> Minimum rating: </Typography>
@@ -117,8 +138,17 @@ const SearchPage: React.FC = () => {
                                 setValue(newValue);
                             }}
                         />
+                        {value !== null && (
+                            <Button
+                                variant="outlined"
+            
+                                onClick={() => setValue(null)}
+                            >
+                                Clear Rating
+                            </Button>)}
 
                     </Grid>
+
                     <Grid item xs={12}>
                         <Button
                             variant="contained"
@@ -136,32 +166,37 @@ const SearchPage: React.FC = () => {
                     Search Results
                 </Typography>
                 <Grid container spacing={3}>
-                    
+
                     {isLoading && <div>Loading...</div>}
                     {isError && <div>Error loading movies</div>}
                     {movies?.map((movie) => (
                         <Grid item key={movie.id} xs={12}>
-                        <Card sx={{display: 'flex'}}>
-                            <CardMedia
-                                component="img"
-                                alt={movie.title}
-                                width="140"
-                               
-                                image={movie.poster_path}
-                                sx={{ flex: '10%' }}
-                            />
-                            <CardContent sx={{ flex: '90%' }}>
-                                <Typography variant="h6">{movie.title}</Typography>
-                                <Typography variant="body2">
-                                    Rating: {movie.vote_average} / 10 | Year: {new Date(movie.release_date).getFullYear()}
-                                    <br /> 
-                                    <br /> 
-                            
-                                    {movie.overview}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
+                            <Card sx={{ display: 'flex' }}>
+                                <CardMedia
+                                    component="img"
+                                    alt={movie.title}
+                                    width="140"
+
+                                    image={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                                    sx={{    
+                                        flex: '10%',
+                                        maxWidth: '15%', //image scaling weirdness forced my hand, but maybe this works
+                                        height: 'auto',
+                                        
+                                    }}
+                                />
+                                <CardContent sx={{ flex: '90%' }}>
+                                    <Typography variant="h6">{movie.title}</Typography>
+                                    <Typography variant="body2">
+                                        Rating: {movie.vote_average} / 10 | Year: {new Date(movie.release_date).getFullYear()}
+                                        <br />
+                                        <br />
+
+                                        {movie.overview}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
                     ))}
                 </Grid>
             </Box>
